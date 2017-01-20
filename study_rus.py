c8e1b02fa2a2d8619f3f417ad3b986514021e508
Asya Simonyan <anniesim97@gmail.com>
2017-01-20 15:16:01 +0300

    added remember me functionality

--- study_rus.py
+++ study_rus.py
@@ -65,17 +65,20 @@ def register():
   #      return render_template('start_page.html')
    # return redirect(url_for('index'))
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('start_page.html')
     username = request.form['username']
     password = request.form['password']
+    remember_me = False
+    if 'remember_me' in request.form:
+        remember_me = True
     registered_user = User.query.filter_by(username=username, password=password).first()
     if registered_user is None:
         flash('Username or Password is invalid', 'error')
         return redirect(url_for('login'))
-    login_user(registered_user)
+    login_user(registered_user, remember=remember_me)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
 
